067dd57c9e6af1ce5db8816e4ea9082e8bd13180
LATerada <larissaakiterada@gmail.com>
2023-06-18 11:08:57 -0300

    :sparkles: Create your-book-list app

---
+++ src/constants/bookList.jsx
@@ -0,0 +1,114 @@
+export const bookList = [
+  {
+    title: "Harry Potter e a Pedra Filosofal",
+    author: "J.K. Rowling",
+    description:
+      "A história do jovem bruxo Harry Potter que descobre seu destino mágico ao ingressar na Escola de Magia e Bruxaria de Hogwarts.",
+    image: "https://exemplo.com/imagem-harry-potter.jpg",
+  },
+  {
+    title: "O Senhor dos Anéis",
+    author: "J.R.R. Tolkien",
+    description:
+      "Uma épica história de fantasia que acompanha a jornada do hobbit Frodo Baggins para destruir um anel maligno e salvar a Terra-média da escuridão.",
+    image: "https://exemplo.com/imagem-senhor-dos-aneis.jpg",
+  },
+  {
+    title: "Cem Anos de Solidão",
+    author: "Gabriel García Márquez",
+    description:
+      "Uma saga familiar que atravessa várias gerações, explorando temas como amor, solidão, magia e realismo fantástico.",
+    image: "https://exemplo.com/imagem-cem-anos-de-solidao.jpg",
+  },
+  {
+    title: "O Código Da Vinci",
+    author: "Dan Brown",
+    description:
+      "Um thriller que combina arte, simbologia e conspiração, enquanto o professor de simbologia Robert Langdon tenta desvendar um segredo oculto.",
+    image: "https://exemplo.com/imagem-codigo-da-vinci.jpg",
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    description:
+      "Um romance distópico que retrata um futuro totalitário e vigilante, onde o protagonista luta contra um governo opressor e manipulador.",
+    image: "https://exemplo.com/imagem-1984.jpg",
+  },
+  {
+    title: "Orgulho e Preconceito",
+    author: "Jane Austen",
+    description:
+      "Um romance clássico que narra a história de Elizabeth Bennet e seu relacionamento com o rico Sr. Darcy no cenário da sociedade inglesa do século XIX.",
+    image: "https://exemplo.com/imagem-orgulho-preconceito.jpg",
+  },
+  {
+    title: "O Alquimista",
+    author: "Paulo Coelho",
+    description:
+      "Uma história espiritual que segue a jornada de um jovem pastor em busca de seu tesouro pessoal, descobrindo lições sobre a vida e o destino.",
+    image: "https://exemplo.com/imagem-o-alquimista.jpg",
+  },
+  {
+    title: "O Hobbit",
+    author: "J.R.R. Tolkien",
+    description:
+      "Uma aventura fantástica que precede os eventos de O Senhor dos Anéis, acompanhando Bilbo Bolseiro em sua jornada com um grupo de anões para recuperar um tesouro roubado por um dragão.",
+    image: "https://exemplo.com/imagem-o-hobbit.jpg",
+  },
+  {
+    title: "A Revolução dos Bichos",
+    author: "George Orwell",
+    description:
+      "Uma fábula satírica que retrata a revolta dos animais de uma fazenda contra seus donos humanos, explorando temas como poder, corrupção e manipulação política.",
+    image: "https://exemplo.com/imagem-a-revolucao-dos-bichos.jpg",
+  },
+  {
+    title: "O Grande Gatsby",
+    author: "F. Scott Fitzgerald",
+    description:
+      "Um clássico da literatura americana que retrata a vida extravagante e decadente dos ricos na década de 1920, abordando temas como amor, ambição e ilusão.",
+    image: "https://exemplo.com/imagem-o-grande-gatsby.jpg",
+  },
+  {
+    title: "Dom Quixote",
+    author: "Miguel de Cervantes",
+    description:
+      "Uma das maiores obras da literatura espanhola, que narra as aventuras do cavaleiro sonhador Dom Quixote e seu fiel escudeiro Sancho Pança.",
+    image: "https://exemplo.com/imagem-dom-quixote.jpg",
+  },
+  {
+    title: "O Sol é para Todos",
+    author: "Harper Lee",
+    description:
+      "Um romance poderoso que aborda questões de racismo e injustiça através dos olhos de Scout, uma jovem garota do sul dos Estados Unidos, durante a década de 1930.",
+    image: "https://exemplo.com/imagem-o-sol-e-para-todos.jpg",
+  },
+  {
+    title: "A Metamorfose",
+    author: "Franz Kafka",
+    description:
+      "Uma novela que conta a história de Gregor Samsa, um homem que acorda transformado em um inseto e precisa lidar com as consequências dessa metamorfose.",
+    image: "https://exemplo.com/imagem-a-metamorfose.jpg",
+  },
+  {
+    title: "Moby Dick",
+    author: "Herman Melville",
+    description:
+      "Um épico literário que segue a obsessão do capitão Ahab em caçar a baleia branca Moby Dick, explorando temas como destino, vingança e natureza humana.",
+    image: "https://exemplo.com/imagem-moby-dick.jpg",
+  },
+  {
+    title: "O Morro dos Ventos Uivantes",
+    author: "Emily Brontë",
+    description:
+      "Um romance gótico que se passa na sombria paisagem dos charnecas de Yorkshire, contando a história de amor e vingança entre Heathcliff e Catherine Earnshaw.",
+    image: "https://exemplo.com/imagem-o-morro-dos-ventos-uivantes.jpg",
+  },
+  {
+    title: "A Sangue Frio",
+    author: "Truman Capote",
+    description:
+      "Um livro de não ficção que relata o brutal assassinato de uma família no Kansas e a subsequente investigação, trazendo à tona questões sobre a natureza do mal.",
+    image: "https://exemplo",
+  },
+];
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { BookForm } from "../components/BookForm";
+import { BookList } from "../components/BookList";
+
+const HomePage = () => {
+  return (
+    <>
+      <BookForm />
+      <BookList />
+    </>
+  );
+};
+export default HomePage;
