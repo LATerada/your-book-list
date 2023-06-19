7b7fb1acd622f2c060e2c277c605760c61c46233
LATerada <larissaakiterada@gmail.com>
2023-06-19 00:17:25 -0300

    :construction: Make delete botton functional

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,10 +3,17 @@ import { BookCard } from "../components/BookCard";
 export const BookList = (props) => {
   const { list, setList } = props;
 
+  const deleteBook = (item) => {
+    const newList = list.filter((bookInfo) => bookInfo !== item);
+    setList(newList);
+  };
+  
   return (
     <>
       {list.map((item, index) => {
-        return <BookCard key={index} item={item}></BookCard>;
+        return (
+          <BookCard key={index} item={item} deleteBook={deleteBook}></BookCard>
+        );
       })}
     </>
   );
