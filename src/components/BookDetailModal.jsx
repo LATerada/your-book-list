a7dbc625ec0011feed52437b1da83c1c9a3c03c6
LATerada <larissaakiterada@gmail.com>
2023-06-19 15:18:14 -0300

    :lipstick: Update BookDetailModal style

--- src/components/BookDetailModal.jsx
+++ src/components/BookDetailModal.jsx
@@ -10,7 +10,7 @@ export const BookDetailModal = (props) => {
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg md:max-w-3xl">
               <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
                   <img
