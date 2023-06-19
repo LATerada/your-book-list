faea8d33d82d1c157b96dce57fd1d6bd66e3ad04
LATerada <larissaakiterada@gmail.com>
2023-06-18 22:44:49 -0300

    :construction: Add BookCard component

--- src/constants/booksList.jsx
+++ src/constants/booksList.jsx
@@ -4,111 +4,114 @@ export const booksList = [
     author: "J.K. Rowling",
     description:
       "A história do jovem bruxo Harry Potter que descobre seu destino mágico ao ingressar na Escola de Magia e Bruxaria de Hogwarts.",
-    image: "https://exemplo.com/imagem-harry-potter.jpg",
+    image: "https://m.media-amazon.com/images/I/61jgm6ooXzL.jpg",
   },
   {
     title: "O Senhor dos Anéis",
     author: "J.R.R. Tolkien",
     description:
       "Uma épica história de fantasia que acompanha a jornada do hobbit Frodo Baggins para destruir um anel maligno e salvar a Terra-média da escuridão.",
-    image: "https://exemplo.com/imagem-senhor-dos-aneis.jpg",
+    image: "https://m.media-amazon.com/images/I/71ZLavBjpRL.jpg",
   },
   {
     title: "Cem Anos de Solidão",
     author: "Gabriel García Márquez",
     description:
       "Uma saga familiar que atravessa várias gerações, explorando temas como amor, solidão, magia e realismo fantástico.",
-    image: "https://exemplo.com/imagem-cem-anos-de-solidao.jpg",
+    image: "https://m.media-amazon.com/images/I/817esPahlrL.jpg",
   },
   {
     title: "O Código Da Vinci",
     author: "Dan Brown",
     description:
       "Um thriller que combina arte, simbologia e conspiração, enquanto o professor de simbologia Robert Langdon tenta desvendar um segredo oculto.",
-    image: "https://exemplo.com/imagem-codigo-da-vinci.jpg",
+    image:
+      "https://books.google.com.br/books/content?id=gV1nhvItuoAC&hl=pt-BR&pg=PA1&img=1&zoom=3&bul=1&sig=ACfU3U2cW5kVj6HxpQNi8gkPIJXJLyD9uQ&w=1280",
   },
   {
     title: "1984",
     author: "George Orwell",
     description:
       "Um romance distópico que retrata um futuro totalitário e vigilante, onde o protagonista luta contra um governo opressor e manipulador.",
-    image: "https://exemplo.com/imagem-1984.jpg",
+    image:
+      "https://m.media-amazon.com/images/I/51VXYaKO-sL._SY344_BO1,204,203,200_QL70_ML2_.jpg",
   },
   {
     title: "Orgulho e Preconceito",
     author: "Jane Austen",
     description:
       "Um romance clássico que narra a história de Elizabeth Bennet e seu relacionamento com o rico Sr. Darcy no cenário da sociedade inglesa do século XIX.",
-    image: "https://exemplo.com/imagem-orgulho-preconceito.jpg",
+    image:
+      "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRgON4QqzU0akM4o0H-3J97qyY1cKzeOHGaUnaXO24mE-7qoc47zJdVKJ7tMF4IP8iI-09OkrMamgzAK6W3uBUXHuqtNb-1VdvxIcgUH0o&usqp=CAE",
   },
   {
     title: "O Alquimista",
     author: "Paulo Coelho",
     description:
       "Uma história espiritual que segue a jornada de um jovem pastor em busca de seu tesouro pessoal, descobrindo lições sobre a vida e o destino.",
-    image: "https://exemplo.com/imagem-o-alquimista.jpg",
+    image: "https://m.media-amazon.com/images/I/51cF7jnQGBS._SY344_BO1,204,203,200_QL70_ML2_.jpg",
   },
   {
     title: "O Hobbit",
     author: "J.R.R. Tolkien",
     description:
       "Uma aventura fantástica que precede os eventos de O Senhor dos Anéis, acompanhando Bilbo Bolseiro em sua jornada com um grupo de anões para recuperar um tesouro roubado por um dragão.",
-    image: "https://exemplo.com/imagem-o-hobbit.jpg",
+    image: "https://leuvendeu.com.br/wp-content/uploads/2021/12/9788578276300.jpg",
   },
   {
     title: "A Revolução dos Bichos",
     author: "George Orwell",
     description:
       "Uma fábula satírica que retrata a revolta dos animais de uma fazenda contra seus donos humanos, explorando temas como poder, corrupção e manipulação política.",
-    image: "https://exemplo.com/imagem-a-revolucao-dos-bichos.jpg",
+    image: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRevNpNxB3J9K8G-bELWV-R0Ym7FOrpktn8IVvsTy1lpza227prOSiX-wgaL8OK9fq6pv_xDVScBao3t-hwIuhqsRuIvtEyVGCbK2R8cwbS",
   },
   {
     title: "O Grande Gatsby",
     author: "F. Scott Fitzgerald",
     description:
       "Um clássico da literatura americana que retrata a vida extravagante e decadente dos ricos na década de 1920, abordando temas como amor, ambição e ilusão.",
-    image: "https://exemplo.com/imagem-o-grande-gatsby.jpg",
+    image: "https://d38h3sy5jr28pf.cloudfront.net/capas-livros/9788580444971-f-scott-fitzgerald-o-grande-gatsby-3473135521.jpg",
   },
   {
     title: "Dom Quixote",
     author: "Miguel de Cervantes",
     description:
       "Uma das maiores obras da literatura espanhola, que narra as aventuras do cavaleiro sonhador Dom Quixote e seu fiel escudeiro Sancho Pança.",
-    image: "https://exemplo.com/imagem-dom-quixote.jpg",
+    image: "https://m.media-amazon.com/images/I/71MnClIKzkL.jpg",
   },
   {
     title: "O Sol é para Todos",
     author: "Harper Lee",
     description:
       "Um romance poderoso que aborda questões de racismo e injustiça através dos olhos de Scout, uma jovem garota do sul dos Estados Unidos, durante a década de 1930.",
-    image: "https://exemplo.com/imagem-o-sol-e-para-todos.jpg",
+    image: "https://m.media-amazon.com/images/I/51wdOrz6uNL._SX340_BO1,204,203,200_.jpg",
   },
   {
     title: "A Metamorfose",
     author: "Franz Kafka",
     description:
       "Uma novela que conta a história de Gregor Samsa, um homem que acorda transformado em um inseto e precisa lidar com as consequências dessa metamorfose.",
-    image: "https://exemplo.com/imagem-a-metamorfose.jpg",
+    image: "https://m.media-amazon.com/images/I/51rvNLi6xyL._SY344_BO1,204,203,200_QL70_ML2_.jpg",
   },
   {
     title: "Moby Dick",
     author: "Herman Melville",
     description:
       "Um épico literário que segue a obsessão do capitão Ahab em caçar a baleia branca Moby Dick, explorando temas como destino, vingança e natureza humana.",
-    image: "https://exemplo.com/imagem-moby-dick.jpg",
+    image: "https://m.media-amazon.com/images/I/51cQOAZhjgL._SY344_BO1,204,203,200_QL70_ML2_.jpg",
   },
   {
     title: "O Morro dos Ventos Uivantes",
     author: "Emily Brontë",
     description:
       "Um romance gótico que se passa na sombria paisagem dos charnecas de Yorkshire, contando a história de amor e vingança entre Heathcliff e Catherine Earnshaw.",
-    image: "https://exemplo.com/imagem-o-morro-dos-ventos-uivantes.jpg",
+    image: "https://m.media-amazon.com/images/I/51mJJ0VPmIL._SY344_BO1,204,203,200_QL70_ML2_.jpg",
   },
   {
     title: "A Sangue Frio",
     author: "Truman Capote",
     description:
       "Um livro de não ficção que relata o brutal assassinato de uma família no Kansas e a subsequente investigação, trazendo à tona questões sobre a natureza do mal.",
-    image: "https://exemplo",
+    image: "https://m.media-amazon.com/images/I/414lUqKIoCL._SX331_BO1,204,203,200_.jpg",
   },
 ];
