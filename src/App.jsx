78f1b57e8eebb38068e98960dbd7caabfff81cac
LATerada <larissaakiterada@gmail.com>
2023-06-19 15:06:44 -0300

    :lipstick: Add bg image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Header } from "./components/Header";
 import HomePage from "./pages/HomePage";
 
 const App = () => {
-  return <HomePage />;
+  return (
+    <div className=" bg-bookshelf">
+      <Header />
+      <HomePage />;
+    </div>
+  );
 };
 
 export default App;
