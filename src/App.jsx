4105c16d3ee3389357264b353282b7ee670bdcd4
LATerada <larissaakiterada@gmail.com>
2023-06-19 15:56:47 -0300

    :lipstick: Update App responsivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
-    <div className=" bg-bookshelf">
+    <div className="bg-bookshelf">
       <Header />
       <HomePage />;
+      <Footer />
     </div>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,8 @@
-export const Footer = () => {};
+export const Footer = () => {
+  return (
+    <div className="h-10 text-center font-semibold p-2 bg-transparent">
+      <span className="italic text-white">Created by </span>
+      <a href="https://github.com/LATerada" className="text-white hover:underline-offset-2">LATerada</a>
+    </div>
+  );
+};
